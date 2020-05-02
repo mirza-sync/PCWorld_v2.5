17f2d82c76e36aef948c6b1708c88147fc3bf307
mirza-sync <mirzasyahmi123@gmail.com>
2020-05-02 17:04:06 +0800

    connection manager mysql

--- src/pcworld/connection/ConnectionManager.java
+++ src/pcworld/connection/ConnectionManager.java
@@ -3,10 +3,12 @@ package pcworld.connection;
 import java.sql.*;
 public class ConnectionManager {
 	static Connection conn;
-	private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
-	private static final String DB_CONNECTION ="jdbc:oracle:thin:@localhost:1521:xe";
-	private static final String DB_USER = "pc";
-	private static final String DB_PASSWORD = "pc";
+//	private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
+//	private static final String DB_CONNECTION ="jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String DB_CONNECTION ="jdbc:mysql://localhost:3306/pcworld?serverTimezone=UTC";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "";
 	public static Connection getConnection() {
 		try {
 			Class.forName(DB_DRIVER);
